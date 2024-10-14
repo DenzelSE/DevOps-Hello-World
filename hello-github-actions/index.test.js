0c1a8c5478e369f136d1f4abf00c21d215caa84c
dselokela023 <dselokela023@student.wethinkco.de>
2024-10-15 00:24:05 +0200

    index on the test

--- hello-github-actions/index.test.js
+++ hello-github-actions/index.test.js
@@ -1,5 +1,4 @@
-const hello = require('/index');
-
-test('return the correct message', () =>{
-    expect(hello()).toBe('Hello, Github Actions!')
-})
+const hello = require('./index');
+test('returns the correct message', () => {
+  expect(hello()).toBe('Hello, GitHub Actions!');
+});
